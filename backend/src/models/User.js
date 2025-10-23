19da95eea61a47cddedb4377e5bcf457c91d2c7d
cuongvt8 <cuongvt8@fpt.com>
2025-10-23 12:19:38 +0700

    fix login authentication

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,48 +1,48 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
-  {
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        lowercase: true,
-    },
-    hashedPassword: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        lơwercase: true,
-    },
-    displayName: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    avatarUrl: {
-        type: String,
-    },
-    avatarId: {
-        type: String,
-    },
-    bio: {
-        type: String,
-        maxlength: 500,
-    },
-    phone: {
-        type: String,
-        sparse: true,
-    },
-},
-  {
-    timestamps: true,
-  }
+   {
+      username: {
+         type: String,
+         required: true,
+         unique: true,
+         trim: true,
+         lowercase: true,
+      },
+      hashedPassword: {
+         type: String,
+         required: true,
+      },
+      email: {
+         type: String,
+         required: true,
+         unique: true,
+         trim: true,
+         lowercase: true,
+      },
+      displayName: {
+         type: String,
+         required: true,
+         trim: true,
+      },
+      avatarUrl: {
+         type: String,
+      },
+      avatarId: {
+         type: String,
+      },
+      bio: {
+         type: String,
+         maxlength: 500,
+      },
+      phone: {
+         type: String,
+         sparse: true,
+      },
+   },
+   {
+      timestamps: true,
+   }
 );
 const User = mongoose.model("User", userSchema);
 
